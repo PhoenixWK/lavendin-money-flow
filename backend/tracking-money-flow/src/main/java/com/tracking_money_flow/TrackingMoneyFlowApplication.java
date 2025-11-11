593cc22b8f3a03ccec871c91cfa6d24d7d15387c
Nguyen Phu <xuanphuda@gmail.com>
2025-11-11 22:14:28 +0700

    init: initialize backend folder

---
+++ backend/tracking-money-flow/src/main/java/com/tracking_money_flow/TrackingMoneyFlowApplication.java
@@ -0,0 +1,13 @@
+package com.tracking_money_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingMoneyFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingMoneyFlowApplication.class, args);
+	}
+
+}
---
+++ backend/tracking-money-flow/src/test/java/com/tracking_money_flow/TrackingMoneyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracking_money_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingMoneyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
